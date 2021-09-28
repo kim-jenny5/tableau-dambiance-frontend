af5684d39441dd34153f5a019106d79fd5a1ee65
Jenny Kim <kimj1905@gmail.com>
2021-09-27 22:30:55 -0400

    added JS functionality for app about side panel

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const base_url = `http://localhost:3000`;
 const collectionApiCall = new CollectionApi(base_url);
 
 collectionApiCall.getCollections();
+
+// getting elements from DOM
+const qMark = document.querySelector(".about-btn");
+
+// console.log(qMark);
+qMark.addEventListener("click", function (e) {
+  debugger;
+  //   console.log("Hello");
+});
