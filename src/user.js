6cdbe3aa8bfa9df7bbf7670e08634f6538530e55
Jenny Kim <kimj1905@gmail.com>
2021-09-28 21:19:43 -0400

    save

--- src/user.js
+++ src/user.js
@@ -1,6 +1,7 @@
 // getting elements from DOM
 const qMark = document.querySelector(".about-btn");
-// const about =
+const about = document.querySelector(".about-app");
+const aboutDev = document.querySelector(".about-dev");
 // ------------------
 
 qMark.addEventListener("click", function () {
