a931980fbcc9e6ae24cdf41d2cd3fe4794e04e56
Jenny Kim <kimj1905@gmail.com>
2021-09-29 21:28:36 -0400

    added js functionality to return to 'homepage' if user clicks on logo

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const base_url = `http://localhost:3000`;
 const collectionApiCall = new CollectionApi(base_url);
 const userApiCall = new UserApi(base_url);
 
+const logo = document.querySelectorAll(".logo");
+
 collectionApiCall.getCollections();
 userApiCall.getUsers();
+
+for (const word of logo) {
+  word.addEventListener("click", function () {
+    if (startingPg.style.display === "none") {
+      startingPg.style.display = "block";
+      newUserPg.style.display = "none";
+      returningUserPg.style.display = "none";
+    }
+  });
+}
