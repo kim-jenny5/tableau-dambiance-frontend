bf485fcc06a37a1bc31b17759ea415432b98107a
Jenny Kim <kimj1905@gmail.com>
2021-10-07 11:51:21 -0400

    dynamically created delete collection button upon access

--- src/collection.js
+++ src/collection.js
@@ -1,4 +1,7 @@
 const xCollectionsBtn = document.querySelector(".close-collections-btn");
+const deleteForm = document.createElement("form");
+const deleteBtn = document.createElement("button");
+
 // const renameTitle = document.querySelector(".renaming-form");
 
 class Collection {
@@ -59,8 +62,8 @@ class Collection {
   // accessCollection(collection) {
   accessCollection = (e) => {
     window.scrollTo(0, 0);
-    e.stopPropagation();
-    // e.stopImmediatePropagation();
+    if (deleteBtn) deleteBtn.remove();
+    // e.stopPropagation();
     // const self = this;
 
     pictureBoard.innerHTML = "";
@@ -99,24 +102,70 @@ class Collection {
       this.addNewPicture();
     });
 
-    this.deleteCollection();
+    this.deleteCollection(e);
   };
 
-  deleteCollection() {
-    const deleteForm = document.querySelector(".delete-form");
-    deleteForm.style.display = "block";
-    console.log(this);
-    const self = this;
+  deleteCollection = (e) => {
+    // const deleteForm = document.querySelector(".delete-form");
+    // deleteForm.style.display = "block";
+
+    deleteForm.setAttribute("class", "delete-form");
+    deleteForm.setAttribute("id", `deleteColl${this.id}`);
+
+    deleteBtn.setAttribute("form", `deleteColl${this.id}`);
+    deleteBtn.setAttribute("type", "submit");
+    deleteBtn.setAttribute("class", "btn btn-outline-dark delete fw-bold");
+    deleteBtn.setAttribute("id", `deleteColl${this.id}`);
+
+    deleteBtn.innerHTML = `<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    class="bi bi-trash"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+    />
+    <path
+      fill-rule="evenodd"
+      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+    />
+  </svg>
+  Delete Entire Collection
+`;
+    mainPg.appendChild(deleteBtn);
+    // console.log(deleteBtn);
+    // debugger;
+
+    // console.log(e.target);
+    // const correctCollection = e.target.innerText;
+
     deleteForm.addEventListener("submit", (e) => {
       e.preventDefault();
+      //   e.stopImmediatePropagation();
+      //   console.log(correctCollection);
+      console.log(this);
       debugger;
-      if (window.confirm("Are you sure you want to delete this collection?")) {
-        console.log(this);
-        console.log(e.target);
-        debugger;
-      }
+      //   if (window.confirm("Are you sure you want to delete this collection?")) {
+      //     // console.log(this);
+      //     console.log(e.target);
+      //     debugger;
+      //   }
     });
-  }
+  };
+
+  // deletingForm = (e) => {
+  //   e.preventDefault();
+  //   // console.log(this);
+  //   // debugger;
+  //   if (window.confirm("Are you sure you want to delete this collection?")) {
+  //     // console.log(this);
+  //     console.log(e.target);
+  //     debugger;
+  //   }
+  // };
 }
 
 // addNewPicture() {
